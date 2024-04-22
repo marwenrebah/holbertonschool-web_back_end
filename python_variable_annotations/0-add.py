0818a0ed6383623eef1afd1faada605b89c1ac52
marwenrebah <6863@holbertonstudents.com>
2024-04-22 09:39:53 -1100

    Basic annotations - add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Basic annotations - add"""
 
+
 def add(a: float, b: float) -> float:
-    """Add two floating-point numbers and returns: 
+    """
+    Add two floating-point numbers and returns:
     float: The sum of the two numbers.
     """
     return a + b
