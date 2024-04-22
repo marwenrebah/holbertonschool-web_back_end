6abc66b0754b2c8765c24beaa38fe9bbfae9bd79
marwenrebah <6863@holbertonstudents.com>
2024-04-22 09:51:05 -1100

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Calculate the floor of a floating-point number.
+    """
+    return math.floor(n)
