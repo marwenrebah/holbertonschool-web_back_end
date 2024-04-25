7b40e5c5ecc1766c78db57aeac94040eaad41593
marwenrebah <6863@holbertonstudents.com>
2024-04-25 01:39:31 -1100

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,8 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Asynchronous generator function that yields random numbers with a 1-second delay."""
+    """Asynchronous generator function that yields
+    random numbers with a 1-second delay."""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
