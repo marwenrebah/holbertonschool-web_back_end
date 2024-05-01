c99c73eacefe95998326c8c466a2980a2abe7e8e
marwenrebah <6863@holbertonstudents.com>
2024-05-01 04:47:01 -1100

    Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -2,6 +2,7 @@
 """ 1-simple_pagination """
 import csv
 from typing import List
+import math
 
 
 class Server:
@@ -41,14 +42,14 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Return a dictionary containing the following key-value pairs"""
         data = self.get_page(page, page_size)
-        total_pages = len(self.dataset()) / page_size
-        if len(self.dataset()) % page_size > 0:
-            total_pages += 1
+        total_pages = math.ceil(len(self.dataset()) / page_size)
         return {
             "page_size": len(data),
             "page": page,
             "data": data,
-            "next_page": page + 1 if page < total_pages else None,
+            "next_page": (
+                page + 1 if len(data) == page_size and page < total_pages else None
+            ),
             "prev_page": page - 1 if page > 1 else None,
-            "total_pages": int(total_pages),
+            "total_pages": total_pages,
         }
