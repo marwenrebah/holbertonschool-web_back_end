80ddca01dc1cc447b657e6f51e2039e9c13f8228
marwenrebah <6863@holbertonstudents.com>
2024-05-01 04:43:27 -1100

    Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ 1-simple_pagination """
 import csv
-from typing import List, Dict
-import math
+from typing import List
 
 
 class Server:
@@ -23,31 +22,33 @@ class Server:
 
         return self.__dataset
 
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """
+        Return a tuple of size two containing a start
+        index and an end index.
+        """
+        start = (page - 1) * page_size
+        end = page * page_size
+        return (start, end)
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Get the page of the dataset"""
-        csv_data = self.dataset()
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-        start, end = index_range(page, page_size)
-        return csv_data[start:end]
+        start, end = self.index_range(page, page_size)
+        return self.dataset()[start:end]
 
-
-def index_range(page: int, page_size: int) -> tuple:
-    """Return a tuple of size two containing a start index and an end index."""
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
-
-
-def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-    """Return a dictionary containing the following key-value pairs"""
-    data = self.get_page(page, page_size)
-    total_pages = math.ceil(len(self.dataset) / page_size)
-    return {
-        "page_size": len(data),
-        "page": page,
-        "data": data,
-        "next_page": page + 1 if len(data) == page_size else None,
-        "prev_page": page - 1 if page > 1 else None,
-        "total_pages": total_pages,
-    }
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Return a dictionary containing the following key-value pairs"""
+        data = self.get_page(page, page_size)
+        total_pages = len(self.dataset()) / page_size
+        if len(self.dataset()) % page_size > 0:
+            total_pages += 1
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": int(total_pages),
+        }
