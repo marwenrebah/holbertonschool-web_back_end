0bd535bad1e39e9e31a2842c8a245572d2e93fd9
marwenrebah <6863@holbertonstudents.com>
2024-05-02 03:08:10 -1100

    Change school topics

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Python function that changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name"""
+    return mongo_collection.updateMany({"name": name}, {"$set": {"topics": topics}})
