26ec850bdad23bfc4f4646a3cf8c12fe1214b50d
marwenrebah <6863@holbertonstudents.com>
2024-05-06 02:04:46 -1100

    fix

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,32 +1,15 @@
 module.exports = {
   env: {
-    browser: false,
-    es6: true,
+    browser: true,
+    es2021: true,
   },
-  extends: [
-    'airbnb-base',
+  extends: 'airbnb-base',
+  overrides: [
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
   },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
 };
