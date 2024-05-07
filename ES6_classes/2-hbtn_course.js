9f0d841ea6a672a9a5ffebbc9669de29a14e5cbd
marwenrebah <6863@holbertonstudents.com>
2024-05-07 09:56:36 -1100

    A Course, Getters, and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -13,16 +13,20 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
-
   get name() {
     return this._name;
   }
-
+  set name(newName) {
+    this._name = newName;
+  }
   get length() {
     return this._length;
   }
-
+  set length(newLength) {
+    this._length = newLength;
+  }
   get students() {
     return this._students;
   }
+  
 }
