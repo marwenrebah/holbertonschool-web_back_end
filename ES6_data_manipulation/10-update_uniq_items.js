ab5ae771495bbcd89692531c06a6bae3f5cad19a
marwenrebah <6863@holbertonstudents.com>
2024-05-29 01:21:42 -1100

    a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1.

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -4,7 +4,7 @@ export default function updateUniqueItems(map) {
   }
 
   for (const [key] of map) {
-    if (map.get(key) !== 1) map.set(key, 100);
+    if (map.get(key) === 1) map.set(key, 100);
   }
   return map;
 }
