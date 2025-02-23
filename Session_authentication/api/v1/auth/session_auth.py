69a39df089eb63da3563c1015a6a22458c9dd342
marwenrebah <6863@holbertonstudents.com>
2025-02-23 15:38:09 +0000

    Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -14,13 +14,15 @@ class SessionAuth(Auth):
 
         if user_id is None or not isinstance(user_id, str):
             return None
-        sessionId = str(uuid4())
-        self.user_id_by_session_id[sessionId] = user_id
-        return sessionId
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """user_id_for_session_id that returns a User ID based on a Session ID"""
+        """user_id_for_session_id that
+        returns a
+        User ID based on a Session ID"""
 
-        if session_id is None or type(session_id) != str:
+        if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
