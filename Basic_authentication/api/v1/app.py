214a9207d8980452d714bc1d1d732806126ac343
marwenrebah <6863@holbertonstudents.com>
2025-02-23 14:30:17 +0000

    pep8

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,13 +7,13 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
+from api.v1.auth.auth import Auth, BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = Auth
+auth = Auth()
 
 
 @app.errorhandler(404)
@@ -40,19 +40,17 @@ def access(error) -> str:
 @app.before_request
 def before_request():
     """before_request function"""
-    AUTH_TYPE = getenv("AUTH_TYPE")
-    auth = Auth(AUTH_TYPE)
-    exist = auth.require_auth(
-        request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
-    authorization_header = auth.authorization_header(request)
-    if authorization_header is None:
-        abort(401)
-    current_user = auth.current_user(request)
-    if current_user is None:
-        abort(403)
+    if auth is not None and auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
+    AUTH_TYPE = getenv("AUTH_TYPE")
+    if AUTH_TYPE == 'basic_auth':
+        auth = BasicAuth()
     app.run(host=host, port=port)
