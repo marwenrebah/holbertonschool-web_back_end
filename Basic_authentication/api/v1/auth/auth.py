0554a63b8c107dc5a64a0ed661a3574c73282eb5
marwenrebah <6863@holbertonstudents.com>
2025-02-23 14:46:17 +0000

    pep8

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -2,10 +2,10 @@
 """manage the API authentication"""
 
 from flask import request
-from typing import List, TypeVar, Union
+from typing import List, TypeVar
 
 
-class Auth:
+class Auth():
     """Auth Class"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
@@ -13,16 +13,17 @@ class Auth:
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
         for i in range(len(excluded_paths)):
-            if excluded_paths[i].rstrip("/") == path.rstrip("/"):
+            if excluded_paths[i].rstrip('/') == path.rstrip('/'):
                 return False
         return True
 
-    def authorization_header(self, request=None) -> Union[str, None]:
+    def authorization_header(self, request=None) -> str:
         """authorization_header method"""
         if request is None:
             return None
-        if request.headers:
-            return request[request.headers["Authorization"]]
+        if "Authorization" in request.headers:
+            return request.headers["Authorization"]
 
-    def current_user(self, request=None) -> TypeVar("User"):
+    def current_user(self, request=request) -> TypeVar('User'):
         """current_user method"""
+        return None
