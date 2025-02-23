c8943e4a28b20411090da96238a2ff10bfa68c54
marwenrebah <6863@holbertonstudents.com>
2025-02-23 13:21:18 +0000

    Error handler: Unauthorized

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,13 +14,14 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
+@app.errorhandler(404, endpoint='not_found')
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401, endpoint='unauthorized')
 def unauthorized(error) -> str:
     """ Unauthorized handler
     """
