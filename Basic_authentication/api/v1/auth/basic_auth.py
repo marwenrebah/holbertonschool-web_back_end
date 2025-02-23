9b87cb3b018fa7196eb6093dd56bd1508e9eed66
marwenrebah <6863@holbertonstudents.com>
2025-02-23 14:52:06 +0000

    Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 import re
+import base64
 
 
 class BasicAuth (Auth):
@@ -10,13 +11,27 @@ class BasicAuth (Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """extract_base64_authorization_header that returns the Base64
-        part of the Authorization header for a Basic Authentication"""
+        """extract_base64_authorization_header that returns the Base64 part
+        of the Authorization header for a Basic Authentication"""
 
-        if authorization_header is None or (not isinstance(
-                authorization_header, str)):
+        if authorization_header is None or (type(authorization_header) != str):
             return None
         if authorization_header.startswith("Basic ") is False:
             return None
         Base64 = re.split(' ', authorization_header)
         return Base64[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header that returns the
+        decoded value of a Base64 string base64_authorization_header"""
+
+        if (base64_authorization_header is None or
+                (type(base64_authorization_header) != str)):
+            return None
+        try:
+            decodeBytes = base64.b64decode(base64_authorization_header)
+            decodedStr = decodeBytes.decode("utf-8")
+            return decodedStr
+        except ValueError:
+            return None
