79d02803a3a880300c23024cc0a81a6497b232de
marwenrebah <6863@holbertonstudents.com>
2025-02-23 12:51:11 +0000

    pep8

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,26 @@
 
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """__init__ method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format method to filter values in incoming log records"""
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record), self.SEPARATOR
+        )
 
 
 def filter_datum(
