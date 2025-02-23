d1d5df6f745a8cf89f998b388984261cd42a2c84
marwenrebah <6863@holbertonstudents.com>
2025-02-23 15:03:44 +0000

    Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,34 +6,33 @@ import re
 import base64
 
 
-class BasicAuth(Auth):
+class BasicAuth (Auth):
     """BasicAuth class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """extract_base64_authorization_header that returns the Base64
         part of the Authorization header for a Basic Authentication"""
 
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
             return None
         if not authorization_header.startswith("Basic "):
             return None
-        base64_part = re.split(" ", authorization_header)
-        return base64_part[1]
+        Base64 = re.split(' ', authorization_header)
+        return Base64[1]
 
-    def decode_base64_authorization_header(
-        self, base64_authorization_header: str
-    ) -> str:
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
         """decode_base64_authorization_header that returns the
-        decoded value of a Base64 string"""
+        decoded value of a Base64 string base64_authorization_header"""
 
-        if base64_authorization_header is None or not isinstance(
-            base64_authorization_header, str
-        ):
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
             return None
         try:
-            decoded_string = base64.b64decode(base64_authorization_header)
-            decoded_string = decoded_string.decode("utf-8")
-            return decoded_string
+            decodeBytes = base64.b64decode(base64_authorization_header)
+            decodedStr = decodeBytes.decode("utf-8")
+            return decodedStr
 
         except Exception:
             return None
