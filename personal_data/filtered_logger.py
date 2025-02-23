b4af797037ec6b02629351b58d359922cfee1b4c
marwenrebah <6863@holbertonstudents.com>
2025-02-23 12:48:20 +0000

    chmod

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """filter_datum function"""
     for field in fields:
         message = re.sub(
-            f"{field}=.*?{separator}",
-            f"{field}={redaction}{separator}",
-            message
+            f"{field}=.*?{separator}", f"{field}={redaction}{separator}", message
         )
 
     return message
