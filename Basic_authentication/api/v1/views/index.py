b2ace8c7bfc29f9385e34f97fa6a7db9852418e3
marwenrebah <6863@holbertonstudents.com>
2025-02-23 13:31:05 +0000

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,8 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False, endpoint='status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False,
+                 endpoint='status')
 def status() -> str:
     """ GET /api/v1/status
     Return:
