0e750caca7a7990240526e54df1c35c41b4ce30a
marwenrebah <6863@holbertonstudents.com>
2025-02-23 13:35:26 +0000

    Error handler: Unauthorized

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,33 +1,68 @@
 #!/usr/bin/env python3
-""" Module of Index views
 """
-from flask import jsonify, abort
+Route module for the API
+"""
+from os import getenv
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.auth import Auth
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE == "basic_auth":
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
     """
-    return jsonify({"status": "OK"})
+    return jsonify({"error": "Not found"}), 404
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
     """
-    abort(401)
+    return jsonify({"error": "Unauthorized"}), 401
 
 
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+@app.errorhandler(403)
+def access(error) -> str:
+    """ access handler
     """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """before_request function"""
+    if auth is None:
+        return
+    exist = auth.require_auth(
+        request.path,
+        ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
+    if exist is False:
+        return
+    authorization_header = auth.authorization_header(request)
+    if authorization_header is None:
+        abort(401)
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
