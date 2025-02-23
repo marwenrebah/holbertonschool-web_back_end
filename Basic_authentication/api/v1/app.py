f6ec2762a78b40e7faf0eee2440c233f8c96b27b
marwenrebah <6863@holbertonstudents.com>
2025-02-23 13:36:49 +0000

    Error handler: Unauthorized

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,20 +7,11 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-
-AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == "basic_auth":
-    auth = BasicAuth()
-else:
-    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -35,31 +26,6 @@ def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def access(error) -> str:
-    """ access handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request():
-    """before_request function"""
-    if auth is None:
-        return
-    exist = auth.require_auth(
-        request.path,
-        ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'])
-    if exist is False:
-        return
-    authorization_header = auth.authorization_header(request)
-    if authorization_header is None:
-        abort(401)
-    current_user = auth.current_user(request)
-    if current_user is None:
-        abort(403)
 
 
 if __name__ == "__main__":
