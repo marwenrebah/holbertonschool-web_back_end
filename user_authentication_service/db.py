b676e084b92ba0dc6250f67cea15eae3410d2938
marwenrebah <6863@holbertonstudents.com>
2025-03-02 17:49:18 +0000

    update user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -13,25 +13,28 @@ from user import Base
 
 
 class DB:
-    """DB class"""
+    """DB class
+    """
 
-    def __init__(self):
-        """Initialize a new DB instance"""
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
-        """Memoized session object"""
+    def _session(self) -> Session:
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Implement the add_user method"""
+        """add_user method that returns a user object"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
