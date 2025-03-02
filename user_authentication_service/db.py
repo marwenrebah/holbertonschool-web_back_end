428235b6dfe263dfdd2d92603861a67df2f1b849
marwenrebah <6863@holbertonstudents.com>
2025-03-02 17:45:03 +0000

    Find user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -9,15 +9,13 @@ from sqlalchemy.orm.session import Session
 from user import User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
-
 from user import Base
 
 
 class DB:
     """DB class"""
 
-    def __init__(self) -> None:
+    def __init__(self):
         """Initialize a new DB instance"""
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -25,7 +23,7 @@ class DB:
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def _session(self):
         """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -33,18 +31,19 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add_user method that returns a user object"""
+        """Implement the add_user method"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """find_user_by method that return a user object based on the email"""
+        """takes in arbitrary keyword arguments and returns the
+        first row found in the users table as filtered by
+        the method’s input arguments."""
         if kwargs is None:
             raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs)
-        for i in user:
-            if i is not None:
-                return i
-        raise NoResultFound
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
